e9c07444eb85fb2df856ab2c9a1f68b0d35801c7
Chandesh LILLMOND <72606214+Chandesh08@users.noreply.github.com>
2021-04-07 20:12:00 +0400

    Final

---
+++ www/services/resultservice.php
@@ -0,0 +1,33 @@
+<?php
+
+
+//access control headears
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header("Access-Control-Allow-Headers: X-Requested-With");
+
+
+
+//creating constants which are parametric access to the database
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', '');
+DEFINE ('DB_HOST', 'localhost');
+DEFINE ('DB_NAME', 'utmdb');
+
+	//the connection string object
+    $conn = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: '.mysqli_connect_error() );
+    
+    $sql = "SELECT * FROM results";
+    $result = mysqli_query($conn, $sql);
+    $rows = array();
+
+    if(mysqli_num_rows($result) > 0){
+        while($r = mysqli_fetch_assoc($result)){
+            array_push($rows, $r);
+        }
+        print json_encode($rows);
+    } else{
+        echo "No Data";
+    }
+    mysqli_close($conn);
+?>
